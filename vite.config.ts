57102cc1444810966a3fc716bb71514e917309d5
carmnk <52402366+carmnk@users.noreply.github.com>
2024-07-24 15:18:52 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [(react as any)()],
-})
+  target: 'esnext',
+  build: {
+    rollupOptions: {
+      external: ['@cmk/fe_utils'],
+    },
+  },
+} as any)
